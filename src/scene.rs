741dd336a1e14a7aed213cb54de5e8a46977cca0
viljokass <vili.karkkainen@outlook.com>
2024-03-29 01:56:12 +0200

    Added phong shading and now more stuff is up to the scene

--- src/scene.rs
+++ src/scene.rs
@@ -6,14 +6,10 @@ pub enum SDF {
 }
 
 impl SDF {
-    pub fn evaluate(&self, p: &Vec3) -> f64{
+    pub fn evaluate(&self, p: &Vec3) -> f64 {
         match self {
-            SDF::Sphere { pos, rad } => {
-                &p.sub(&pos).length() - rad
-            }
-            SDF::Plane { pos, nor, h } => {
-                &p.sub(pos).dot(&nor) + h
-            }
+            SDF::Sphere { pos, rad } => &p.sub(&pos).length() - rad,
+            SDF::Plane { pos, nor, h } => &p.sub(pos).dot(&nor) + h,
         }
     }
 }
@@ -21,12 +17,17 @@ impl SDF {
 // Scene struct
 pub struct Scene {
     pub lpos: Vec3,
+    pub cpos: Vec3,
+
+    pub lcol: Vec3,
+    pub acol: Vec3,
+
     pub objs: Vec<SDF>,
 }
 
 // Scene methods
 impl Scene {
-    pub fn eval(&self, p: &Vec3) -> f64{
+    pub fn eval(&self, p: &Vec3) -> f64 {
         let mut min = f64::MAX;
         let objs = &self.objs;
         for v in objs {
--- src/vecmath.rs
+++ src/vecmath.rs
@@ -6,12 +6,8 @@ pub struct Vec3 {
 }
 
 impl Vec3 {
-    pub fn from(x: f64, y: f64, z: f64) -> Vec3{
-        Vec3 {
-            x,
-            y,
-            z,
-        }
+    pub fn from(x: f64, y: f64, z: f64) -> Vec3 {
+        Vec3 { x, y, z }
     }
 
     pub fn add(&self, oth: &Vec3) -> Vec3 {
@@ -30,19 +26,19 @@ impl Vec3 {
         }
     }
 
-    pub fn length(&self) -> f64{
+    pub fn length(&self) -> f64 {
         let x = self.x;
         let y = self.y;
         let z = self.z;
-        f64::sqrt(x*x+y*y+z*z)
+        f64::sqrt(x * x + y * y + z * z)
     }
 
     pub fn normalize(&self) -> Vec3 {
         let len = self.length();
         Vec3 {
-            x: self.x/len,
-            y: self.y/len,
-            z: self.z/len,
+            x: self.x / len,
+            y: self.y / len,
+            z: self.z / len,
         }
     }
 
@@ -56,5 +52,19 @@ impl Vec3 {
 
     pub fn dot(&self, oth: &Vec3) -> f64 {
         self.x * oth.x + self.y * oth.y + self.z * oth.z
+    }
+
+    pub fn reflect(&self, normal: &Vec3) -> Vec3 {
+        let dp = self.dot(&normal);
+        let minus = normal.scale(2.0 * dp);
+        self.sub(&minus)
+    }
+
+    pub fn times(&self, oth: &Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x * oth.x,
+            y: self.y * oth.y,
+            z: self.z * oth.z,
+        }
     }
 }
