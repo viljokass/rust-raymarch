e335318388f7f5f46c60ae58ecdc8682ae7b405f
viljokass <vili.karkkainen@outlook.com>
2024-03-29 17:23:49 +0200

    Optimized so that when reflecting, the scene doesn't get copied

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use png;
-use scene::{Scene, SDF, Obj, Material};
 use std::fs::File;
 use std::io::{self, BufWriter};
 use std::path::Path;
-use vecmath::Vec3;
 
 // A module where screen is set up
 mod screen;
@@ -52,65 +50,6 @@ fn ask_dimension(dim: Dimension) -> u32 {
     }
 }
 
-// Create a scene - hardcoded, but has been split in
-// such a way that creating from file shouldn't be too hard.
-fn setup_scene() -> Scene {
-    let mut objs: Vec<Obj> = Vec::new();
-
-    let s1 = Obj {
-        sdf: SDF::Sphere {
-            pos: Vec3::from(-1.5, 0., -2.),
-            rad: 1.,
-        },
-        mat: Material::Color {
-            col: Vec3::from(1., 0.5, 0.2),
-        },
-    };
-    objs.push(s1);
-
-    let s2 = Obj {
-        sdf: SDF::Sphere {
-            pos: Vec3::from(1., 2., 0.),
-            rad: 1.,
-        },
-        mat: Material::Reflect,
-    };
-    objs.push(s2);
-
-    let s3 = Obj {
-        sdf: SDF::Sphere {
-            pos: Vec3::from(2., -1., -1.),
-            rad: 1.,
-        },
-        mat: Material::Color {
-            col: Vec3::from(1., 0.6, 0.6),
-        }
-    };
-    objs.push(s3);
-
-    let p1 = Obj {
-        sdf: SDF::Plane {
-            pos: Vec3::from(0., -3., 0.),
-            nor: Vec3::from(0., 1., 0.),
-            h: 0.01,
-        },
-        mat: Material::Color {
-            col: Vec3::from(0.1, 0.1, 0.3),
-        },
-    };
-    objs.push(p1);
-
-    Scene {
-        lpos: Vec3::from(0., 2., -2.),
-        cpos: Vec3::from(0., 0., -5.),
-
-        lcol: Vec3::from(1., 0.6, 0.4),
-        acol: Vec3::from(0., 0., 0.),
-
-        objs: objs,
-    }
-}
-
 // Create an image file and save something onto it.
 fn main() {
     // Ask the image dimensions from the user
@@ -120,7 +59,7 @@ fn main() {
     // For debugging purposes
     println!("Dimensions of the image is {}x{}.", width, height);
 
-    let scene = setup_scene();
+    let scene = scene::create_scene();
 
     // Set the path, file and buffer writer
     let path = Path::new(r"./img.png");
